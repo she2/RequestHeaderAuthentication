c807aedc1289cb6d6b6a94c66beca997a0a4429a
Shittu Olugbenga <she2gbenga@gmail.com>
2020-07-14 17:06:21 +0100

    Added Readme

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # RequestHeaderAuthentication
 An authentication scheme that helps validate/authenticate given http request header value in a clean way.
+
+#### Usage
+Usage is as simple as 
+- Add to your `ConfigureServices` method in startup class 
+
+
+```
+ public IServiceProvider ConfigureServices(IServiceCollection services)
+ {
+           services.AddAuthentication()
+                      .AddRequestHeaderAuthentication(RequestHeaderDefault.AUTHENTICATION_SCHEME_TWO, op =>
+                        {
+                            //specify the class to call for validating the request headers
+                            op.Events = new SomeAuthEvents();
+
+                            // The claim issuer
+                            op.ClaimsIssuer = AuthenticationKeys.CLAIMS_ISSUER;
+                            
+                            // The header keys to validate
+                            op.HeaderKey = new[] {
+                                "X-ApiKey",
+                                "X-Another-ApiKkey"
+                            };
+                        });
+ }
+
+ 
+```
+
+- Create an event class that inherit from `RequestHeaderAuthenticationEvents` and override `ValidateToken` method
+
+```
+public class SomeAuthEvents : RequestHeaderAuthenticationEvents
+    {
+
+        public override async Task ValidateToken(ValidateTransXTokenContext context)
+        {       
+            // validate each request header here
+        }
+    }
+
+```
+- Add `app.UseHeaderAuth(TransXAuthDefault.AUTHENTICATION_SCHEME_ONE);` to the `Configure` method of the Startup class.
+
+Please note that `TransXAuthDefault.AUTHENTICATION_SCHEME_ONE` can be replaced with any string but it must be same as what was specified in `ConfigureServices`.
+
+### Header Validation
+
+To validate a token, access the index from the collection of tokens via the `ValidateTransXTokenContext.TokenDetails` property.
+
+e.g `TokenDetails tokenDetail = context.TokenDetails[i];` where `i` is the index of the token to be accessed.
+
+- *A valid header*
+
+If an header is valid, 
+
+1. Set the `TokenValid` property of the selected token to `true`. For example,  `tokenDetail.TokenValid = false;` 
+2. Create a set of claims, if required, to be added to the `ValidateTransXTokenContext`'s `ClaimsToSet` property . For example, `context.ClaimsToSet.AddRange(userClaims);`
+
+- *An invalid header*
+
+if an header is not valid,
+
+1. Set the `TokenValid` property of the selected token to `false`. For example,  `tokenDetail.TokenValid = false;` 
+2. Optionally set an error message on the `ValidateTransXTokenContext`'s `ErrorMessage` property . For example, `context.ErrorMessage = "some error message";`
+
+
+If any of the tokens have the `TokenValid` property set to false, the validation will fail with a 403 error containing the error messages specified.
--- RequestHeaderAuthentication/Extensions/RequestHeaderAuthenticationExtension.cs
+++ RequestHeaderAuthentication/Extensions/RequestHeaderAuthenticationExtension.cs
@@ -10,41 +10,41 @@ namespace RequestHeaderAuthentication
     public static class RequestHeaderAuthenticationExtension
     {
         /// <summary>
-        /// This defaults to use <see cref="TransXAuthDefault.AUTHENTICATION_SCHEME_ONE"/>
+        /// This defaults to use <see cref="RequestHeaderDefault.AUTHENTICATION_SCHEME_ONE"/>
         /// </summary>
         /// <param name="builder"></param>
         /// <returns>Returns <see cref="AuthenticationBuilder"/> used to configure the authentication</returns>
-        public static AuthenticationBuilder AddTransXAuth(this AuthenticationBuilder builder)
-           => builder.AddTransXAuth(RequestHeaderDefault.AUTHENTICATION_SCHEME_ONE, _ => { });
+        public static AuthenticationBuilder AddRequestHeaderAuthentication(this AuthenticationBuilder builder)
+           => builder.AddRequestHeaderAuthentication(RequestHeaderDefault.AUTHENTICATION_SCHEME_ONE, _ => { });
 
         /// <summary>
-        /// This defaults to using <see cref="TransXAuthDefault.AUTHENTICATION_SCHEME_ONE"/>
+        /// This defaults to using <see cref="RequestHeaderDefault.AUTHENTICATION_SCHEME_ONE"/>
         /// </summary>
         /// <param name="builder"></param>
         /// <param name="configureOptions"> An <see cref="Action" /> used to configure <see cref="RequestHeaderAuthenticationOptions"/></param>
         /// <returns>Returns <see cref="AuthenticationBuilder"/> used to configure the authentication</returns>
-        public static AuthenticationBuilder AddTransXAuth(this AuthenticationBuilder builder, Action<RequestHeaderAuthenticationOptions> configureOptions)
-            => builder.AddTransXAuth(RequestHeaderDefault.AUTHENTICATION_SCHEME_ONE, configureOptions);
+        public static AuthenticationBuilder AddRequestHeaderAuthentication(this AuthenticationBuilder builder, Action<RequestHeaderAuthenticationOptions> configureOptions)
+            => builder.AddRequestHeaderAuthentication(RequestHeaderDefault.AUTHENTICATION_SCHEME_ONE, configureOptions);
 
         /// <summary>
         /// Allows you to use any predefined or custom authentication scheme
         /// </summary>
         /// <param name="builder"></param>
-        /// <param name="authenticationScheme">One of the <see cref="TransXAuthDefault"/> constants or a custom authentication schedme</param>
+        /// <param name="authenticationScheme">One of the <see cref="RequestHeaderDefault"/> constants or a custom authentication schedme</param>
         /// <param name="configureOptions"> An <see cref="Action" /> used to configure <see cref="RequestHeaderAuthenticationOptions"/></param>
         /// <returns>Returns <see cref="AuthenticationBuilder"/> used to configure the authentication</returns>
-        public static AuthenticationBuilder AddTransXAuth(this AuthenticationBuilder builder, string authenticationScheme, Action<RequestHeaderAuthenticationOptions> configureOptions)
-            => builder.AddTransXAuth(authenticationScheme, displayName: null, configureOptions: configureOptions);
+        public static AuthenticationBuilder AddRequestHeaderAuthentication(this AuthenticationBuilder builder, string authenticationScheme, Action<RequestHeaderAuthenticationOptions> configureOptions)
+            => builder.AddRequestHeaderAuthentication(authenticationScheme, displayName: null, configureOptions: configureOptions);
 
         /// <summary>
         /// Allows you to use any predefined or custom authentication scheme
         /// </summary>
         /// <param name="builder"></param>
-        /// <param name="authenticationScheme">One of the <see cref="TransXAuthDefault"/> constants or a custom authentication schedme</param>
+        /// <param name="authenticationScheme">One of the <see cref="RequestHeaderDefault"/> constants or a custom authentication schedme</param>
         /// <param name="displayName"></param>
         /// <param name="configureOptions"> An <see cref="Action" /> used to configure <see cref="RequestHeaderAuthenticationOptions"/></param>
         /// <returns>Returns <see cref="AuthenticationBuilder"/> used to configure the authentication</returns>
-        public static AuthenticationBuilder AddTransXAuth(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<RequestHeaderAuthenticationOptions> configureOptions)
+        public static AuthenticationBuilder AddRequestHeaderAuthentication(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<RequestHeaderAuthenticationOptions> configureOptions)
         {
             builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<RequestHeaderAuthenticationOptions>, RequestHeaderAuthenticationPostConfigureOptions>());
             return builder.AddScheme<RequestHeaderAuthenticationOptions, RequestHeaderAuthenticationHandler>(authenticationScheme, displayName, configureOptions);
